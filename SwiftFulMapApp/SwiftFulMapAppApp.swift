ffeb035e4d3ec00de2a0cacb3d65d2c07e26970e
Diego Rodrigues <diegorodrigues.fgl@gmail.com>
2023-05-14 20:24:28 -0300

    Initial

---
+++ SwiftFulMapApp/SwiftFulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFulMapAppApp.swift
+//  SwiftFulMapApp
+//
+//  Created by Diego Rodrigues on 14/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
